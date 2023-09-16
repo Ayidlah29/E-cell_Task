06b05c8bcac2dc6f3a4eeb6d0859902a36e5ff91
sneha haldiya <127124375+Ayidlah29@users.noreply.github.com>
2023-09-17 00:36:07 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,27 @@
+function clicked() {
+    document.getElementById("btn").style.background = "red";
+}
+function clicked1() {
+    document.getElementById("btn1").style.background = "red";
+}
+function clicked2() {
+    document.getElementById("btn2").style.background = "red";
+}
+function clicked3() {
+    document.getElementById("btn3").style.background = "red";
+}
+function clicked4() {
+    document.getElementById("btn4").style.background = "red";
+}
+function clicked5() {
+    document.getElementById("btn5").style.background = "red";
+}
+function clicked6() {
+    document.getElementById("btn6").style.background = "red";
+}
+function clicked7() {
+    document.getElementById("btn7").style.background = "red";
+}
+function clicked8() {
+    document.getElementById("btn8").style.background = "red";
+}
